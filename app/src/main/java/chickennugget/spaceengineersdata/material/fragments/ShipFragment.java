ca2b2b077f3d933a2c1ce89d813cbed4146487e5
Chicken-Nugget <lenny27g@gmail.com>
2016-01-24 00:36:16 -0500

    Add Ship Fragment buttons

--- app/src/main/java/chickennugget/spaceengineersdata/material/fragments/ShipFragment.java
+++ app/src/main/java/chickennugget/spaceengineersdata/material/fragments/ShipFragment.java
@@ -18,18 +18,42 @@ import chickennugget.spaceengineersdata.material.utils.ViewUtil;
 import chickennugget.spaceengineersdata.material.widgets.Button;
 
 public class ShipFragment extends Fragment implements View.OnClickListener {
-
+    //Ship (Ship, Control, Thrust, [Passenger Seat])
     private static final int[] button_id = {
             R.id.b_gyroscope,
             R.id.b_landing_gear,
             R.id.b_artificial_mass,
             R.id.b_jump_drive,
+            R.id.b_cockpit_screen,
+            R.id.b_cockpit_enclosed,
+            R.id.b_cockpit_small_screen,
+            R.id.b_cockpit_fighter,
+            R.id.b_passenger_seat,
+            R.id.b_control_panel,
+            R.id.b_small_thruster,
+            R.id.b_large_thruster,
+            R.id.b_small_hydrogen_thruster,
+            R.id.b_large_hydrogen_thruster,
+            R.id.b_small_atmospheric_thruster,
+            R.id.b_large_atmospheric_thruster,
     };
     private static final int[] sheet_id = {
             R.layout.f_gyroscope,
             R.layout.f_landing_gear,
             R.layout.f_artificial_mass,
             R.layout.f_jump_drive,
+            R.layout.f_cockpit_screen,
+            R.layout.f_cockpit_enclosed,
+            R.layout.f_cockpit_small_screen,
+            R.layout.f_cockpit_fighter,
+            R.layout.f_passenger_seat,
+            R.layout.f_control_panel,
+            R.layout.f_small_thruster,
+            R.layout.f_large_thruster,
+            R.layout.f_small_hydrogen_thruster,
+            R.layout.f_large_hydrogen_thruster,
+            R.layout.f_small_atmospheric_thruster,
+            R.layout.f_large_atmospheric_thruster,
     };
     private static final Button[] button = new Button[button_id.length];
     private MainActivity mActivity;
