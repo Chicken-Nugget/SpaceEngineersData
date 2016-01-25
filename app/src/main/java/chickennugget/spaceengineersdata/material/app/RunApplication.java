2895d0f7c31fbff0696f62c2c0a7bb298c8d34d2
Chicken-Nugget <lenny27g@gmail.com>
2016-01-25 02:53:25 -0500

    Remove Leak Canary

--- app/src/main/java/chickennugget/spaceengineersdata/material/app/RunApplication.java
+++ app/src/main/java/chickennugget/spaceengineersdata/material/app/RunApplication.java
@@ -1,27 +1,12 @@
 package chickennugget.spaceengineersdata.material.app;
 
 import android.app.Application;
-import android.content.Context;
-
-import com.squareup.leakcanary.LeakCanary;
-import com.squareup.leakcanary.RefWatcher;
-
-import chickennugget.spaceengineersdata.BuildConfig;
 
 public class RunApplication extends Application {
-
-    private RefWatcher refWatcher;
-
-    public static RefWatcher getRefWatcher(Context context) {
-        RunApplication application = (RunApplication) context.getApplicationContext();
-        return application.refWatcher;
-    }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        if (BuildConfig.DEBUG)
-            refWatcher = LeakCanary.install(this);
         ThemeManager.init(this, 2, 0, null);
     }
 }
